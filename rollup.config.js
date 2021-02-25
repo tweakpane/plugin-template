8f4a07df213b399b85cbff508d9e2818f79bd5b4
cocopon <cocopon@me.com>
2021-02-26 07:08:34 +0900

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,71 @@
+import Replace from '@rollup/plugin-replace';
+import Typescript from '@rollup/plugin-typescript';
+import Autoprefixer from 'autoprefixer';
+import NodeSass from 'node-sass';
+import Postcss from 'postcss';
+import Cleanup from 'rollup-plugin-cleanup';
+import { terser as Terser } from 'rollup-plugin-terser';
+
+async function compileCss() {
+	const css = NodeSass.renderSync({
+		file: 'src/sass/plugin.scss',
+		outputStyle: 'compressed',
+	}).css.toString();
+
+	const result = await Postcss([Autoprefixer]).process(css, {
+		from: undefined,
+	});
+	return result.css.replace(/'/g, "\\'").trim();
+}
+
+function getPlugins(css, shouldMinify) {
+	const plugins = [
+		// NOTE: `paths` should be set to avoid unexpected type confliction
+		// https://github.com/Microsoft/typescript/issues/6496
+		Typescript({
+			tsconfig: 'src/ts/tsconfig.json',
+		}),
+		Replace({
+			__css__: css,
+			preventAssignment: false,
+		}),
+	];
+	if (shouldMinify) {
+		plugins.push(Terser());
+	}
+	return [
+		...plugins,
+		// https://github.com/microsoft/tslib/issues/47
+		Cleanup({
+			comments: 'none',
+		}),
+	];
+}
+
+export default async () => {
+	const production = process.env.BUILD === 'production';
+	const postfix = production ? '.min' : '';
+
+	const css = await compileCss();
+	return {
+		input: 'src/ts/plugin.ts',
+		external: ['tweakpane'],
+		output: {
+			file: `dist/tweakpane-plugin-template${postfix}.js`,
+			format: 'umd',
+			globals: {
+				tweakpane: 'Tweakpane',
+			},
+			name: 'TweakpanePluginTemplate',
+		},
+		plugins: getPlugins(css, production),
+
+		// Suppress `Circular dependency` warning
+		onwarn(warning, rollupWarn) {
+			if (warning.code === 'CIRCULAR_DEPENDENCY') {
+				return;
+			}
+			rollupWarn(warning);
+		},
+	};
+};
